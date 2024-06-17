875dbfe484c960ba9db3b78bf5e3803c9fc3c585
Konstantin S. <skg@genialconsult.ru>
2024-06-17 17:20:05 +0300

    Initial commit

---
+++ src/Cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+    String name;
+    int age;
+    double weight;
+
+    Cat(String name, int age, double weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Привет, МИР!");
+        int age=4;
+        double weight=4.5;
+
+    }
+}
